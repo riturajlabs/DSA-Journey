85c2c4163816c0eae35561b6a48c3f51299ed656
riturajlabs <riturajlabs@outlook.com>
2026-07-26 09:25:36 +0530

    Add largest element in array solution

---
+++ Java/01-Arrays/LargestElement.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class LargestElement {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements in the array: ");
+        int n = sc.nextInt();
+
+        int[] array = new int[n];
+        System.out.println("Enter the elements of the array (comma-separated):");
+        for (int i = 0; i < n; i++) {
+            array[i] = sc.nextInt();
+        }
+
+        int largest = findLargestElement(array);
+        System.out.println("The largest element in the array is: " + largest);
+    }
+
+    public static int findLargestElement(int[] array) {
+        int largest = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > largest) {
+                largest = array[i];
+            }
+        }
+        return largest;
+    }
+}
